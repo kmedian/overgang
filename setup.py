2ac44424eefb5cbc98b47d1ac03291be13683735
554c46 <554c46@gmail.com>
2017-07-11 15:05:36 +0200

    minor changes

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='overgang',
-      version='0.1',
+      version='0.1.0',
       description='transition matrix estimation',
       url='http://github.com/ulf1/overgang',
       author='Ulf Hamster',
-      author_email='554c46+overgang@gmail.com',
+      author_email='554c46@gmail.com',
       license='MIT',
       packages=['overgang'],
       zip_safe=False)
